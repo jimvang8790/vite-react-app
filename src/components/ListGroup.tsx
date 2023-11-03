c373b786d1945644908cfaba747fcd89d0500970
jimvang8790 <vangjim15@gmail.com>
2023-11-03 01:10:09 -0500

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Fragment } from "react";
 
 const ListGroup = () => {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //   items.map((item) => {
-  //     <li>{item}</li>;
-  //   });
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  // The benefits of using a function is that functions can have parameters
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null;
+  //   };
   return (
     <>
       <h1>List</h1>
+      {/* {getMessage()} */}
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}{" "}
+      {/* if item.length = 0 then false/nothing will be render on the screen but if item.length > 0 then display <p> */}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
