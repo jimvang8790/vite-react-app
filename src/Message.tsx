14c0be07885276047f0d2edadd208b77b19440e2
jimvang8790 <vangjim15@gmail.com>
2023-11-03 00:22:48 -0500

    adding message component

---
+++ src/Message.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Message = () => {
+    const name = 'Jim';
+    return (
+        <div>Hello Vite Worlds by {name}</div>
+    )
+}
+
+export default Message
+
+// //PascalCasing
+// function Message () {
+//     // JSX: JavaScript XML
+//     return <h1>Hello World</h1>
+// }
